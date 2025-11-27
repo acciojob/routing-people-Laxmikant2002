4adc2923971663b5940b7bd8fdd45b4ab9323ff4
Laxmikant Shinde <laxmikant251102@gmail.com>
2025-11-27 09:58:05 +0530

    task done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './../styles/App.css';
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 
 const App = () => {
   return (
-    <div>
+    <Router>
+      <div>
         {/* Do not remove the main div */}
-    </div>
+        <Switch>
+          <Route exact path="/" component={UserList} />
+          <Route path="/users/:id" component={UserDetails} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
